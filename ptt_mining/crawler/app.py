eed4922ee8c5da518f408e599be8dc76458e9291
Joy  Liao <y02121148s@gmail.com>
2019-02-20 21:24:42 +0800

    Update app.py

--- ptt_mining/crawler/app.py
+++ ptt_mining/crawler/app.py
@@ -13,13 +13,13 @@ def main():
     def search_all():
         for i in range(0, page_number):
             datas = crawl.get_hrefs_from_page()
-            s = saver.to_csv(datas, save_file_path)
+            s = saver.to_csv(datas, '../datasets/ptt.csv')
 
     def select_from_key():
         for i in range(0, page_number):
             datas = crawl.search_from_keyword(keyword)
-            s = saver.to_csv(datas, save_file_path)
-    print(if_search)
+            s = saver.to_csv(datas, '../datasets/ptt.csv')
+
     if if_search == 'true': select_from_key()
     elif if_search == 'false': search_all()
     else: print('please check `IF_SEARCH` of .env file')
@@ -28,7 +28,7 @@ if __name__ == "__main__":
     load_dotenv()
     board = os.getenv("BOARD")
     keyword = os.getenv("KEYWORD")
-    save_file_path = os.getenv("FILE_PATH_NAME")
+    # save_file_path = os.getenv("FILE_PATH_NAME")
     page_number =  os.getenv("PAGE_NUMBER")
     if_search =  os.getenv("IF_SEARCH")
     page_number = int(page_number)
